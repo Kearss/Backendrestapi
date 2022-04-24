c82e34a754fc1779b496e5f6df69cdea4908d439
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-04-24 12:40:29 +0300

    adding frontend
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 app.use(cors());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("pages/index.ejs");
+  });
+
 const mongoose = require("mongoose");
 //Lisätään tietokannan osoite muuttujaan.
 var uri = "mongodb+srv://kears:Password123@cluster0.39rm7.mongodb.net/WEBapplicationRESTAPI?retryWrites=true&w=majority"
 
-mongoose.Promise = global.Promise;
 //Yhdistetään ohjelma tietokantaan
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {console.log("Connected to the database!")})
@@ -18,10 +26,6 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 console.log("Failed to connect to the database.", err)
     process.exit();
 })
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
 
 //Tuodaan reitit erillisestä tiedostosta
 require("./app/routes/routes")(app);
